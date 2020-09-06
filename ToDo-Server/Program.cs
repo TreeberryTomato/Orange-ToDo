58727d37b051dd7016ed2470a84380a1b48dafe5
Chen Weihao <dmt1809254@xmu.edu.my>
2020-09-06 18:12:59 +0800

    Initial Commit, able to send request from webpage, Scheduler to sort and arrange the tasks

---
+++ ToDo-Server/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+using System.Collections.Generic;
+
+namespace ToDo_Server
+{
+    class Program
+    {
+        static void Main()
+        {
+            LinkedList<Task> tasks = new LinkedList<Task>();
+            var task1 = new Task();
+            task1.Content = "Hello";
+
+            System.Threading.Thread.Sleep(2000);
+
+            var task2 = new Task();
+            task2.Content = "World";
+
+            tasks.AddFirst(task2);
+            tasks.AddLast(task1);
+
+            var scheduler = new Scheduler(tasks);
+
+            Console.WriteLine(scheduler.Tasks.First.Value.Content);
+
+        }
+        static void Main1(string[] args)
+        {
+            string prefix = @"http://localhost:8080/v1/taskinfo/";
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add(prefix);
+            listener.Start();
+            while(true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+
+                context.Response.AppendHeader("Access-Control-Allow-Origin", "*");
+
+                Console.WriteLine(request.RawUrl);
+
+                HttpListenerResponse response = context.Response;
+                
+
+                Task task1 = new Task()
+                {
+                    Content = "完成任务1",
+                    StartDateTime = DateTime.Now.ToLocalTime(),
+                    DeadLine = DateTime.Now.ToLocalTime().AddDays(3),
+                    IsDone = false
+                };
+
+                Task task2 = new Task()
+                {
+                    Content = "完成任务2",
+                    StartDateTime = DateTime.Now.ToLocalTime(),
+                    DeadLine = DateTime.Now.ToLocalTime().AddDays(3),
+                    IsDone = false
+                };
+
+                var tasks = new List<Task>(2);
+
+                tasks.Add(task1);
+                tasks.Add(task2);
+                
+
+                string responseString = JsonSerializer.Serialize(tasks);
+
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+
+                // Get a response stream and write the response to it.
+                response.ContentLength64 = buffer.Length;
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+
+                response.Close();
+
+                // You must close the output stream.
+                output.Close();
+                
+            }
+            listener.Stop();
+        }
+    }
+}
---
+++ ToDo-Server/Scheduler.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDo_Server
+{
+    class Scheduler
+    {
+        private LinkedList<Task> _tasks;
+
+        private LinkedList<LinkedList<Task>> _schedule;
+
+        public LinkedList<Task> Tasks
+        {
+            get
+            {
+                return _tasks;
+            }
+            private set
+            {
+                _tasks = value;
+            }
+        }
+
+        public LinkedList<LinkedList<Task>> Schedule
+        {
+            get
+            {
+                return _schedule;
+            }
+            private set
+            {
+                _schedule = value;
+            }
+        }
+
+        public Scheduler()
+        {
+            Tasks = new LinkedList<Task>();
+        }
+
+        public Scheduler(IEnumerable<Task> tasks)
+        {
+            Add(tasks);
+        }
+
+        //Add a task while maintaining the order by StartDateTime
+        public LinkedListNode<Task> Add(Task task)
+        {
+            if( !(Tasks is LinkedList<Task>) )
+            {
+                Tasks = new LinkedList<Task>();
+                return Tasks.AddFirst(task);
+            }
+            //if there is no task in the LinkedList
+            if(Tasks.Count==0)
+            {
+                return Tasks.AddFirst(task);
+            }
+
+            //otherwise add this task to its right position to 
+            //maintain LinkedList ordered by StartDateTime
+            LinkedListNode<Task> cur = Tasks.First;
+            while(cur is LinkedListNode<Task> && 
+                cur.Value.StartDateTime <= task.StartDateTime)
+            {
+                cur = cur.Next;
+            }
+
+            if(cur is LinkedListNode<Task>)
+            {
+                return Tasks.AddBefore(cur, task);
+            }
+
+            return Tasks.AddLast(task);
+
+        }
+
+        //Add a group of tasks by using IEnumerator while maintaining the order
+        public void Add(IEnumerable<Task> tasks)
+        {
+            var cur = tasks.GetEnumerator();
+
+            while(cur.MoveNext())
+            {
+                Add(cur.Current);
+            }
+        }
+
+        private void Arrange()
+        {
+            foreach(var task in Tasks)
+            {
+                var schedule = Schedule.First;
+                while(schedule is LinkedListNode<LinkedList<Task>>)
+                {
+                    //if there is no conflict between the last task in this list
+                    //and the current task, add this task to this list.
+                    if(schedule.Value.Last.Value.DeadLine<=task.StartDateTime)
+                    {
+                        schedule.Value.AddLast(task);
+                        goto NextTask;
+                    }
+                }
+
+                //if this task cannot be put into every existing list,
+                //create a new list to hold it.
+                LinkedList<Task> newList = new LinkedList<Task>();
+                newList.AddFirst(task);
+                Schedule.AddLast(newList);
+
+            NextTask:
+                continue;
+            }
+        }
+    }
+}
